b8fa10fc1f29d819f253344752c6575fc5353731
gfonteno <gfonteno@cisco.com>
2019-10-05 21:45:47 -0500

    app (flask web app)

---
+++ app/run.py
@@ -0,0 +1,145 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponseTable', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    #new visualisation - count per category
+    category_names =  list(df.iloc[:,4:].columns)
+    category_count = (df.iloc[:,4:] != 0).sum().values
+    
+    #sorting the results
+    sortx = [x for _,x in sorted(zip(category_count, category_names))]
+    sorty = sorted(category_count)    
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts,
+                    text=genre_counts,
+                    textposition = 'auto',
+                    marker={'color': genre_counts, 
+                                           'colorscale': 'Jet'}
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=sortx,
+                    y=sorty,
+                    text=sorty,
+                    textposition = 'auto',
+                    marker=dict(
+                        color='rgb(100,202,225)',
+                        line=dict(
+                        color='rgb(72,61,139)',
+                        width=1),
+                    ),
+                    opacity=0.8
+                )
+            ],
+
+            'layout': {
+                'title': 'Messages per Categoriy Distribution',
+                'yaxis': {
+                    'title': "Count per Category"
+                },
+                'xaxis': {
+                    'title': "Category Name",
+                    'tickangle': 45
+                },
+                'height': 500,
+                'margin': dict(
+                    #t = 100,
+                    b = 150, 
+                )
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
